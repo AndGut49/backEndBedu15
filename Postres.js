52b5a93e219fd84e1abf3c483de22d0335dcd6c4
AndGut <andie.gtz00@gmail.com>
2021-02-25 21:51:52 -0600

    Clase de la entidad Postres

---
+++ Postres.js
@@ -0,0 +1,9 @@
+class Postre{
+    constructor(id, nombre, precio, stock, calificacion){
+        this.id =id;
+        this.nombre =nombre;
+        this.precio =precio;
+        this.stock= stock;
+        this.calificacion = calificacion;
+    }
+}
