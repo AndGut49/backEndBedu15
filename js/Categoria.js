156b8d8f560becc43d547640874c6fe15424bc87
Victor Lascares <vmlascares@gmail.com>
2021-02-26 10:20:36 -0600

    Agregando la clase Categoria y Tamaño

---
+++ js/Categoria.js
@@ -0,0 +1,22 @@
+class Categoria {
+    constructor (id, nombre, descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.descripcion = descripcion;
+    }
+
+    crearCategoria () {
+        // Función para crear una nueva categoria
+    }
+    actualizarCategoria () {
+        // Función para actualizar la información de una categoria
+    }
+    eliminarCategoria () {
+        //Función para eliminar una categoria
+    } 
+
+    consultarCategorias () {
+        //Función para consultar las categorias que se tienen
+    }
+}
