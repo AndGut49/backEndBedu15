2133c5e9cb34d67b9d9a181d0ae9f91f21ce9c19
VictorLascares <vmlascares@gmail.com>
2021-02-28 19:41:36 -0600

    Eliminando archivo Postres.js

--- Postres.js
+++
@@ -1,9 +0,0 @@
-class Postre{
-    constructor(id, nombre, precio, stock, calificacion){
-        this.id =id;
-        this.nombre =nombre;
-        this.precio =precio;
-        this.stock= stock;
-        this.calificacion = calificacion;
-    }
-}
