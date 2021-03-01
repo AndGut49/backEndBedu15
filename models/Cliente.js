acd49bda5a7d5b697fc814ea41df148bc60afb2e
VictorLascares <vmlascares@gmail.com>
2021-02-28 19:39:28 -0600

    Agregando modelo de Cliente Postre Usuario y Tamanio

---
+++ models/Cliente.js
@@ -0,0 +1,11 @@
+class Cliente {
+    constructor(nombre, apellido, direccion, cp, correo, pago) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.cp = cp;
+        this.correo = correo;
+        this.pago = pago;
+    }
+}
---
+++ models/Usuario.js
@@ -0,0 +1,30 @@
+class Usuario{ //Usuari
+	constructor(userId, nombre, apellidoPaterno, apellidoMaterno, foto, edad, sexo, direccion, email, password, telefono){
+        tbis.userId = userId; //id unico de usuario
+		this.nombre = nombre; // nombre del usuario
+        this.apellidoPaterno = apellidoPaterno; //apellido paterno del usuario
+        this.apellidoMaterno = apellidoMaterno; //apellido materno del usuario
+        this.foto = foto; //foto del usuario
+		this.edad = edad; // edad del usuario
+		this.sexo = sexo; // sexo del usuario
+		this.direccion = direccion; // direccion del usuario
+        this.email = email; //email del usuario
+        this.password = password; //contraseña para logearse
+        this.telefono = telefono; //telefono
+	}
+	agregarUsuario(){
+		// función para guardar un nuevo registro en la base de datos.
+	}
+    modificarUsuario(){
+
+    }
+    consultarUsuarios(){
+
+    }
+    eliminarUsuario(){
+
+    }
+    login(){
+        
+    }
+}
