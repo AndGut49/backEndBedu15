ddd2ed42d963eaeae4cd6b5d3e046be5d0a20fb7
Rafael Santos <rast.santos@gmail.com>
2021-03-06 11:07:32 -0600

    Corrigiendo email en cliente

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,12 +1,12 @@
 class Cliente {
-    constructor(idCliente, nombre, apellidoPaterno, apellidoMaterno, direccion, cp, correo, password) {
+    constructor(idCliente, nombre, apellidoPaterno, apellidoMaterno, direccion, cp, email, password) {
         this.idCliente = idCliente;
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
         this.direccion = direccion;
         this.cp = cp;
-        this.correo = correo;
+        this.email = email;
         this.password = password;
     }
 }
