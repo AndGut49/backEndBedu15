fc18da71c29b58d692760c0d198a1e2df5d2474a
AndGut49 <73410357+AndGut49@users.noreply.github.com>
2021-03-05 23:12:42 -0600

    Inserts para tabla tamanio

---
+++ Inserts_tamanio.sql
@@ -0,0 +1,5 @@
+INSERT INTO tamanio VALUES (1, "Individual");
+INSERT INTO tamanio VALUES (2, "Chico");
+INSERT INTO tamanio VALUES (3, "Mediano");
+INSERT INTO tamanio VALUES (4, "Grande");
+SELECT * from tamanio;
