ad6a4cc871695038c32f90b184012ae42e5eaef3
Victor Lascares <vmlascares@gmail.com>
2021-03-06 16:28:56 -0600

    Completando 10 categorias

--- sql/Inserts_categoria.sql
+++ sql/Inserts_categoria.sql
@@ -4,4 +4,8 @@ INSERT INTO categoria VALUES(3,"Galletas");
 INSERT INTO categoria VALUES(4,"Pay");
 INSERT INTO categoria VALUES(5,"Tartas");
 INSERT INTO categoria VALUES(6,"Panques");
+INSERT INTO categoria VALUES(7,"Helados");
+INSERT INTO categoria VALUES(8,"Flan");
+INSERT INTO categoria VALUES(9,"Paletas de Hielo");
+INSERT INTO categoria VALUES(10,"Crepas");
 SELECT * from categoria
