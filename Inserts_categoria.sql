2cdcf83e833a6c84d98853b688a1e0134a8a9b47
AndGut49 <73410357+AndGut49@users.noreply.github.com>
2021-03-05 23:11:11 -0600

    Inserts para la tabla categoria

---
+++ Inserts_categoria.sql
@@ -0,0 +1,7 @@
+INSERT INTO categoria VALUES(1,"Pasteles");
+INSERT INTO categoria VALUES(2,"Gelatinas");
+INSERT INTO categoria VALUES(3,"Galletas");
+INSERT INTO categoria VALUES(4,"Pay");
+INSERT INTO categoria VALUES(5,"Tartas");
+INSERT INTO categoria VALUES(6,"Panques");
+SELECT * from categoria
