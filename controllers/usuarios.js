4dd2f1ef354cb23f73d667c90a9b1d7b9d450a59
Pamela Navarro Herrera <pamela.navarroha@gmailcom>
2021-03-11 17:20:00 -0600

    Primera versión de controlador de Usuario

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,7 @@
 /*  Archivo controllers/usuarios.js
  *  Simulando la respuesta de objetos Usuario
  *  en un futuro aquí se utilizarán los modelos
- */
+ 
 
 // importamos el modelo de usuarios
 const Usuario = require('../models/Usuario')
@@ -65,4 +65,66 @@ module.exports = {
   modificarUsuario,
   eliminarUsuario,
   login
+} */
+
+// Importamos modelo Usuario
+
+const Usuario = require('../models/Usuario')
+
+// Función para crear nuevo registro de Usuario
+
+function crearUsuario(req, res,next) {
+  // construye una instancia del modelo Usuario con los argumentos que recibe en la petición
+  const usr = Usuario.build(req.body)
+  // Guarda esta instancia, es hasta este momento que se modifica la base de datos.
+  usr.save().then(user => {
+    return res.status(201).json(user.toAuthJSON())
+  }).catch(next);
+}
+
+// Función para obtener Usuarios
+
+function obtenerUsuarios(req, res) {
+  // Hace una consulta en la base de datos.
+  User.findAll().then(users => {
+    return res.json(users)
+  }).catch(error => {
+    return res.sendStatus(401)
+  })
+}
+
+// Función para búsquedas más específicas
+
+
+
+// Función para modificar Usuario
+
+function modificarUsuario(req, res,next) {
+  // Se crea un usuario con el id del que se quiere modificar y los cambios descritos en el body
+  const usr = User.create({
+    id : req.params.id,
+    ...req.body
+  })
+  // Se guarda en la DB
+  usr.save().then(user => {
+    return res.status(201).json(user.toAuthJSON())
+  }).catch(next);
+}
+
+// Función para eliminar Usuario
+
+function eliminarUsuario(req, res) {
+  // Usamos findByPK para buscar al usuario por su id
+  const usr = User.findByPk(req.usuario.id);
+  if (usr === null){
+    // si no existe lanzamos un 400 
+    return res.sendStatus(401)
+  } else {
+    // Si existe, lo eliminamos
+    usr.destroy().then(usr => {
+      return res.status(200)
+    }).catch(err => {
+      return res.sendStatus(500)
+    })
+  }
 }
